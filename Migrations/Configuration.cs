de1a9f1866ef865925a9a7bb650f377fce5ae09b
Priyanka <Priyanka@DESKTOP-E8J08GK>
2024-07-16 18:26:12 +0530

    Add project files.

---
+++ Migrations/Configuration.cs
@@ -0,0 +1,28 @@
+﻿namespace EntityFrameworkMain.Migrations
+{
+    using EntityFrameworkMain.Models;
+    using System;
+    using System.Collections.Generic;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<EntityFrameworkMain.Models.RecordContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(EntityFrameworkMain.Models.RecordContext context)
+        {
+            var student = new List<Student>
+           {new Student {Id = 1,    Name="Priyanka" },
+           new Student {Id = 2, Name="SONA" },
+
+           };
+            student.ForEach(s=> context.Students.Add(s));
+            context.SaveChanges();
+        }
+    }
+}
---
+++ Models/RecordContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace EntityFrameworkMain.Models
+{
+    public class RecordContext : DbContext
+    {
+         public RecordContext() : base("RecordContext")
+        {
+
+        }
+        public DbSet<Student> Students { get; set; }    
+    }
+}
